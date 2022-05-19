f5a757573f1e7dc3dde9d64d3f2a18cb43cbdb1f
smartboy0310 <qdustudent6@gmail.com>
2022-05-19 14:13:09 +0500

    Create Gallery page

--- src/Components/Contact/ContactLocation.jsx
+++ src/Components/Contact/ContactLocation.jsx
@@ -8,6 +8,9 @@ function ContactLocation() {
 		<>
 			<section className="contact-location">
 				<div className="container">
+					<h2 className="contact-location__heading">
+						Bizning joylashuv
+					</h2>
 					<a className="location__link" href="https://2gis.uz/uz/tashkent/firm/70000001036919062?m=69.257373%2C41.323764%2F17.21%2Fp%2F45" target='_blank'>
 						<Image
 							className="location__image"
